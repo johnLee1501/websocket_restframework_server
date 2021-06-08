3f459ae9707ee150b0785f3757825ea7311cdd9f
jhonj <johnvega@wposs.com>
2021-06-08 17:34:37 -0500

    heroku deployment

--- websocket_server/asgi.py
+++ websocket_server/asgi.py
@@ -13,5 +13,4 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websocket_server.settings')
-django.setup()
 application = get_default_application()
