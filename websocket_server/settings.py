651d98cc0478966cab7be4bf5f73a76a3f5b1c31
jhonj <johnvega@wposs.com>
2021-06-10 17:15:22 -0500

    Configuración Docker dev with Redis

--- websocket_server/settings.py
+++ websocket_server/settings.py
@@ -85,7 +85,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('redis://localhost', 6379)],
+            "hosts": ['redis://localhost:6379'],
         },
     },
 }
